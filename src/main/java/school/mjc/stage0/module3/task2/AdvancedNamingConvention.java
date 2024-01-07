57899e611518d2d35294d386ecaf7c3ef2365aac
andsps <spirkevichusandrei@mail.ru>
2024-01-07 19:05:19 +0300

    Some changes

--- src/main/java/school/mjc/stage0/module3/task2/AdvancedNamingConvention.java
+++ src/main/java/school/mjc/stage0/module3/task2/AdvancedNamingConvention.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.module3.task2;
 public class AdvancedNamingConvention {
     private static final int ADULT_AGE = 18;
 
-    private int AGE;
+    private int age;
     private int phoneNumber;
 
     void callToFriend() {
